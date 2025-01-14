4fa5526166b07815f8f27d2658a79330983369a7
klothyan <icecreamy17@gmail.com>
2025-01-14 14:57:14 -0700

    Saved

---
+++ W02_Team_Discount.py
@@ -0,0 +1,18 @@
+import math
+
+subtotal = input("Please enter the subtotal here: ")
+salestax = input("Please enter the sales tax amount here: ")
+
+total_normal= math.ceil (subtotal + salestax)
+
+print(f"Your total is {total_normal:.2f}. Thank you and have a nice day.")
+
+
+
+
+
+
+
+
+
+
