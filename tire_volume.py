cbf55966bcdb1693228e4eee8433b46fcd859fbc
klothyan <icecreamy17@gmail.com>
2025-01-16 15:24:10 -0700

    Working on tire volume

--- tire_volume.py
+++ tire_volume.py
@@ -15,6 +15,23 @@ print(f"The current date is {current_date_and_time:%Y-%m-%d}.")
 with open("volumes.txt", "at") as volumes_file:
     print(f"{current_date_and_time:%Y-%m-%d}, {w:.2f}, {a:.2f}, {d:.2f}, {v:.2f}", file=volumes_file)
 
+buy = input("Would you like to buy some tires with the dimensions you provided? (Y/N)")
+
+if buy.lower == "y":
+    print("Thank you! Your tires have been ordered.")
+    address = input("Please type your address here so we can deliver them to your house: ")
+    print(f"Your address is {address}. Thank you for shopping with us! Have a good day!")
+
+if buy.lower == "n":
+    annoying = input("Are you sure you DON'T want to order this limited time deal? (Y/N)")
+    if annoying.lower == "n":
+        print("Thanks for changing your mind!")
+    if survey_annoying.lower() == "y":
+        print("Have a bad day, goodbye. ")
+
+
+
+
 
 
 
