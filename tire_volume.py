d5005021ded021feb48236ea25769e6aa4b0195b
klothyan <icecreamy17@gmail.com>
2025-01-16 14:56:08 -0700

    Successfully copied answers to volumes.txt file.

--- tire_volume.py
+++ tire_volume.py
@@ -13,14 +13,7 @@ current_date_and_time = datetime.now(tz=None)
 print(f"The current date is {current_date_and_time:%Y-%m-%d}.")
 
 with open("volumes.txt", "at") as volumes_file:
-    print(f"The current date is {current_date_and_time:%Y-%m-%d}.")
-    w = float(input("Please enter the width of the tire in mm (ex 205): "))
-    a = float(input("Please enter the aspect ratio of the tire (ex 60): "))
-    d = float(input("Please enter the diameter of the wheel in inches (ex 15): "))
-
-    v = (math.pi * (w ** 2) * a * (w * a + 2540 * d)) / 10000000000
-
-    print(f"The approximate volume is {v:.2f} liters.")
+    print(f"{current_date_and_time:%Y-%m-%d}, {w:.2f}, {a:.2f}, {d:.2f}, {v:.2f}", file=volumes_file)
 
 
 
