1d98bc8dd448f47507e12bc3932c3b1e371196b6
klothyan <icecreamy17@gmail.com>
2025-01-16 14:49:08 -0700

    Working on 02 Prove assignment.

--- tire_volume.py
+++ tire_volume.py
@@ -7,3 +7,25 @@ d = float(input("Please enter the diameter of the wheel in inches (ex 15): "))
 v = (math.pi * (w ** 2) * a * (w * a + 2540 * d)) / 10000000000
 
 print(f"The approximate volume is {v:.2f} liters.")
+
+from datetime import datetime
+current_date_and_time = datetime.now(tz=None)
+print(f"The current date is {current_date_and_time:%Y-%m-%d}.")
+
+with open("volumes.txt", "at") as volumes_file:
+    print(f"The current date is {current_date_and_time:%Y-%m-%d}.")
+    w = float(input("Please enter the width of the tire in mm (ex 205): "))
+    a = float(input("Please enter the aspect ratio of the tire (ex 60): "))
+    d = float(input("Please enter the diameter of the wheel in inches (ex 15): "))
+
+    v = (math.pi * (w ** 2) * a * (w * a + 2540 * d)) / 10000000000
+
+    print(f"The approximate volume is {v:.2f} liters.")
+
+
+
+
+
+
+
+
